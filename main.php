cfed2506a376c75ceb09d51a057e9102df3b3ffa
yzw <316193839@qq.com>
2016-12-25 22:19:26 +0800

    增加了命令行判断限制

--- main.php
+++ main.php
@@ -1,16 +1,22 @@
 <?php
+//只能在cli模式下运行
+if(php_sapi_name() === 'cli'){
+    echo 'cli 模式启动'."\r\n";
+}else{
+    exit('本程序只能在cli模式下启动');
+}
 $start = time();
 echo '程序开始启动...'."\r\n";
 $handler = [];
-function run($start,$end){
+function run($pro_num){
     global $handler;
-    for($i = $start;$i<$end;$i++){
-        $handler[] = popen('php worker.php '.$i.' '.$end,'r');   
+    for($i = 0;$i<$pro_num;$i++){
+        $handler[] = popen('php worker.php '.$i.' '.$pro_num,'r');   
     }    
 }
 
 //经过反复试验 在AMD X740 4核cpu的情况下 开7个进程速度是最快的180-190s之间
-run(0,7);
+run(7);
 
 foreach ($handler as $prc) {
     //echo fgets($prc);
