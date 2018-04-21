ece85128175ff6b30fe0b9b735719fddf0570acf
vv <316193839@qq.com>
2018-04-21 20:19:39 +0800

    新增了tag功能

--- Worker/worker.php
+++ Worker/worker.php
@@ -1,9 +1,6 @@
 <?php
 require_once "./vendor/autoload.php";
 
-require_once(dirname(__DIR__)."/Config/config.php");
-require_once(dirname(__DIR__)."/Util/function.php");
-
 if(isset($argv[3])){
     $std = new \Worker\Student($argv[3], $argv[4]);
 }else{
--- main.php
+++ main.php
@@ -3,21 +3,20 @@ require_once('./Config/config.php');
 require_once('./Util/function.php');
 require_once('frame.php');
 set_error_handler('errorHandler');
-//ֻ����cliģʽ������
+
 if(php_sapi_name() === 'cli'){
     echo 'cli start'."\r\n";
 }else{
-    exit('mode is wrong!'."\r\n");
+    exit('only for cli!'."\r\n");
 }
 $start = time();
 $shareMode = SHARE_MODE?'memory':'file';
 echo 'share mode is ...'.$shareMode."\r\n";
 
-
 $files = [];
 
 $file_path = "E:\student\/20180419";
-//$file_path = "E:\����\ѧ����ӡ2016\ԭʼ��Ƭ���Ѿ�����";
+
 scanAll($file_path,$files);
 
 $data[0] = $files;
